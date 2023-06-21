1e2c4e7337767be2dae0fdbe3dd11397c544b88e
Mtan1230 <m.tan1230@gmail.com>
2023-06-21 16:35:39 -0400

    add router notes.js to handle get/post request

--- routes/notes.js
+++ routes/notes.js
@@ -1,10 +1,35 @@
 const notes = require('express').Router();
-const fs = require('fs');
+const { readFile, writeFile } = require('fs/promises');
+const { v4: uuidv4 } = require('uuid');
 
-notes.get('/', (req, res) => {
-    fs.readFile('./db/db.json', 'utf8', (err, data) => 
-    err? console.log(err) : res.json(JSON.parse(data))
-    );    
+notes.get('/', async (req, res) => {
+    try {
+        const data = await readFile('./db/db.json', 'utf8');
+        res.json(JSON.parse(data));
+    } catch (err) {
+        console.error(err)
+    }
 });
+
+notes.post('/', async (req, res) => {
+    const { title, text } = req.body;
+    if (title) {
+        const newNote = {
+            title,
+            text,
+            note_id: uuidv4()
+        };
+
+        try {
+            const data = await readFile('./db/db.json', 'utf8');
+            const notes = JSON.parse(data);
+            notes.push(newNote);
+            writeFile('./db/db.json', JSON.stringify(notes));
+            res.json(newNote)
+        } catch (err) {
+            console.error(err)
+        }
+    }
+})
 
 module.exports = notes;
--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const path = require('path');
 
+const api = require('./routes/index');
+
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/api', api);
 
 app.use(express.static('public'));
 
